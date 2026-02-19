6e14ea0530415dd75ddc82aee549d5ce89e70ad8
Davide Poletti <AJ1100267@tdf.hubtotal.net>
2026-02-20 00:13:17 +0100

    solution

---
+++ angular-front-end/src/app/components/users-list/users-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user.interface';
+
+@Component({
+  selector: 'app-users-list',
+  imports: [CommonModule],
+  templateUrl: './users-list.component.html',
+  styleUrl: './users-list.component.css'
+})
+export class UsersListComponent implements OnInit {
+  users = signal<User[]>([]);
+  loading = signal<boolean>(true);
+  error = signal<string | null>(null);
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    this.loading.set(true);
+    this.error.set(null);
+
+    this.userService.getUsers().subscribe({
+      next: (data) => {
+        this.users.set(data);
+        this.loading.set(false);
+      },
+      error: (err) => {
+        this.error.set('Failed to load users. Please try again later.');
+        this.loading.set(false);
+        console.error('Error loading users:', err);
+      }
+    });
+  }
+}
